c0ed4d2a6b814bd04bcd33bcfa43d2eb3399cd02
Blake Sticland <blakelists@yahoo.com.au>
2020-12-14 00:56:58 +1100

    added floppy icon to save plus hover working on save icon; stuck on adding background color to calendar to signify hour

--- script/script.js
+++ script/script.js
@@ -13,37 +13,45 @@
         $("#currentDay").text(currentDayTime);
     }, 1000) ;
 
+// global variables
+
+
+// funcion to generate one row per hour on the schedule
 
 function createTimeRow(time) {
+    
+    // Use timeblocks to display the activities for the day
+    let newRow = $("<div>");
+    newRow.attr("class", "row");
+    
+    let newColTime = $("<div>");
+    newColTime.attr("class", "col col-1 col-sm-2 hour");
+    // TODO: add time
+    newColTime.text(time);
+    
+    let newColActivity = $("<div>");
+    newColActivity.attr("class", "col col-10 col-sm-8");
+    newColActivity.attr("id", "activity-entry");
+    let newActivityText = $("<textarea>");
+    newActivityText.attr("name", "Activity");
+    newActivityText.attr("id", "");
+    newActivityText.attr("cols", "30");
+    newActivityText.attr("rows", "3");
+    newActivityText.attr("placeholder", "Enter activity here");
+     // TODO: add attr
+    newColActivity.append(newActivityText);
 
-// Use timeblocks to display the activities for the day
-let newRow = $("<div>");
-newRow.attr("class", "row");
+    let newColSave = $("<div>");
+    newColSave.attr("class", "col col-1 col-sm-2 saveBtn");
+    let saveBtn = $("<i>");
+    saveBtn.attr("class", "far fa-save");
+    newColSave.append(saveBtn);
 
-let newColTime = $("<div>");
-newColTime.attr("class", "col col-1 col-sm-2 hour");
-// TODO: add time
-newColTime.text(time)
+    newRow.append(newColTime, newColActivity, newColSave);
+    return newRow;
 
-let newColActivity = $("<div>");
-newColActivity.attr("class", "col col-10 col-sm-8");
-let newActivityText = $("<textarea>");
-newActivityText.attr("name", "Activity");
-newActivityText.attr("id", "");
-newActivityText.attr("cols", "40");
-newActivityText.attr("rows", "3");
-// TODO: add attr
-newColActivity.append(newActivityText);
-
-let newColSave = $("<div>");
-newColSave.attr("class", "col col-1 col-sm-2 saveBtn");
-let saveBtn = $("<button>");
-saveBtn.text("Save");
-newColSave.append(saveBtn);
-
-newRow.append(newColTime, newColActivity, newColSave);
-return newRow;
 }
+
 
 let calendarContainer = $("#calendar");
 let times = ['7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm'];
@@ -54,11 +62,43 @@ for (let index = 0; index < times.length; index++) {
 
 
 // TODO: color code the timeblocks to indicate whether it is in the past, present or future
+function createBgColor (ColActivity, index) {
+        // let currentHour = moment().hour();
+    let activityBgColor = $("#activity-entry");
+    activityBgColor.attr("style", "background:pink");
+    let calendar24H = index + 7;
+    let currentHour = 10;
     // ** Classes for the different colors in CSS
+            if (calendar24H < currentHour) {
+                activityBgColor.attr("class", "col col-10 col-sm-8 past");
+                console.log("past" + currentHour);
+                console.log("past" + calendar24H);
+            
+            }
+            else if (calendar24H == currentHour) {
+                activityBgColor.attr("class", "col col-10 col-sm-8 present");
+                console.log("current" + currentHour);
+                console.log("current" + calendar24H);
+            
+            }
+            else {
+                activityBgColor.attr("class", "col col-10 col-sm-8 future");
+                console.log("future" + currentHour);
+                console.log("future" + calendar24H);
+            }
+    }
+
+
+    
+
 
 // TODO: Be able to enter events in the timeblocks
 
-// TODO: Be able to click to save the new event
+
+// TODO: Function to be able to click to save the new event
+$("#saveBtn").click(function save() {
+
+});
 
 // TODO: Be able to refresh the page and the saved data persists. 
 
