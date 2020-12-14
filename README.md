d6da7732455b667989d105605a6675b966de7977
Blake Sticland <blakelists@yahoo.com.au>
2020-12-14 23:42:20 +1100

    activity entry saving to localStorage; need to getItem next and add colors for past, present adn future hour-slots

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
+# Work Day Scheduler
 # work-day-scheduler
+
+## Aim: 
+
+To create a simpple salendar application that enables the userto save events for each hour of the day. The app runs in the browser and features dynamically updatd HTML and CSS powered by jQuery.
+
+## Goals:
+
+* Current day is displayed at the top of the calendar. This is dynamically updated each second to accurately reflect the current day and date. 
+
+* Timeblocks for standard business hours are presented.
+
+* When the user clicks on a timeblock, they can enter an activity (for that particular hour).
+
+* Clicking the "save" icon stores the text for that event in local storage. 
+
+## To be implemented in the future:
+
+* Each timeblock is color coded to indicate whether it is in the past, present, or the future.
+
+* When the page is refreshed, the saved activity entries persist.
+
+
+Screenshot of the implemented Work Day Scheduler:
+
--- script/script.js
+++ script/script.js
@@ -3,17 +3,17 @@
     // "document.ready" makes sure that our JavaScript doesn't get run until the HTML document is finished loading.
     $(document).ready(function() {setInterval(function() {
 
+// Display the current day and date at the top of the calendar
 
-// PSEUDOCODE STEPS
-
-// Display the current day at the top of the calendar
-// To do this, use moment() JS to get the current day and date from computer clock
-
-        let currentDayTime = moment().format('MMMM Do YYYY, h:mm:ss a');
+        let currentDayTime = moment().format('dddd, MMMM Do');
         $("#currentDay").text(currentDayTime);
+        return currentHour;
     }, 1000) ;
+    
 
 // global variables
+    var currentHour = moment().hour();  // note this is outside createTimeRow function
+
 
 
 // funcion to generate one row per hour on the schedule
@@ -24,14 +24,16 @@ function createTimeRow(time) {
     let newRow = $("<div>");
     newRow.attr("class", "row");
     
+    // hour of the day
     let newColTime = $("<div>");
     newColTime.attr("class", "col col-1 col-sm-2 hour");
-    // TODO: add time
     newColTime.text(time);
     
+    // activity
     let newColActivity = $("<div>");
     newColActivity.attr("class", "col col-10 col-sm-8");
     newColActivity.attr("id", "activity-entry");
+
     let newActivityText = $("<textarea>");
     let newActivityTextId = "Activity-" + time;
     newActivityText.attr("id", newActivityTextId);
@@ -39,15 +41,18 @@ function createTimeRow(time) {
     newActivityText.attr("cols", "30");
     newActivityText.attr("rows", "3");
     newActivityText.attr("placeholder", "Enter activity here");
-     // TODO: add attr
+
     newColActivity.append(newActivityText);
 
+    // save button (icon)
     let newColSave = $("<div>");
     newColSave.attr("class", "col col-1 col-sm-2 saveBtn");
     let saveBtn = $("<i>");
     saveBtn.attr("class", "far fa-save");
+
     newColSave.append(saveBtn);
 
+    // add new row
     newRow.append(newColTime, newColActivity, newColSave);
     return newRow;
 
@@ -55,52 +60,132 @@ function createTimeRow(time) {
 
 
 let calendarContainer = $("#calendar");
-let times = ['7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm'];
+let times = ['9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm'];
+
 for (let index = 0; index < times.length; index++) {
     const time = times[index];
     calendarContainer.append(createTimeRow(time));
-}
+};
 
 
 // TODO: color code the timeblocks to indicate whether it is in the past, present or future
-function createBgColor (ColActivity, index) {
-        // let currentHour = moment().hour();
-    let activityBgColor = $("#activity-entry");
-    activityBgColor.attr("style", "background:pink");
-    let calendar24H = index + 7;
-    let currentHour = 10;
+    // let activityEntry = $("#activity-entry");
+    // let calendar24H = index + 9;
     // ** Classes for the different colors in CSS
-            if (calendar24H < currentHour) {
-                activityBgColor.attr("class", "col col-10 col-sm-8 past");
-                console.log("past" + currentHour);
-                console.log("past" + calendar24H);
+    //         if (calendar24H < currentHour) {
+    //             activityEntry.attr("class", "col col-10 col-sm-8 past");
+    //             console.log("past" + currentHour);
+    //             console.log("past" + calendar24H);
             
-            }
-            else if (calendar24H == currentHour) {
-                activityBgColor.attr("class", "col col-10 col-sm-8 present");
-                console.log("current" + currentHour);
-                console.log("current" + calendar24H);
+    //         }
+    //         else if (calendar24H == currentHour) {
+    //             activityEntry.attr("class", "col col-10 col-sm-8 present");
+    //             console.log("current" + currentHour);
+    //             console.log("current" + calendar24H);
             
-            }
-            else {
-                activityBgColor.attr("class", "col col-10 col-sm-8 future");
-                console.log("future" + currentHour);
-                console.log("future" + calendar24H);
-            }
+    //         }
+    //         else {
+    //             activityEntry.attr("class", "col col-10 col-sm-8 future");
+    //             console.log("future" + currentHour);
+    //             console.log("future" + calendar24H);
+    //         }
+
+// TODO: be able to save activity entry to local storage
+    //TODO: be able to see user input in "activity" textarea
+    let userEntry = $("#Activity-9am");
+    // add index to userEntry to target each "activity"
+
+
+    localStorage.setItem("hour", userEntry.val());
+    localStorage.getItem("hour");
+
+
+
+
+
+    let userActivityInput = document.querySelectorAll("textarea");
+
+    var activities = [];
+
+    init();
+
+    function renderActivities() {
+
+        
+
+
+    }
+
+    function init() {
+
+    // Get stored activities from localStorage,
+    // Parsing the JSON string object
+    var storedActivities = JSON.parse(localStorage.getItem("activities"));
+
+    // If activities were retrieved from localStorage,  update the activities array to it
+    if (storedActivities !== null) {
+        activities = storedActivities;
+    }
+
+    // Render activities to the DOM
+    renderActivities();
+
     }
 
 
+
+    function storeActivities() {
+        //Stringify and set "activities" key in localStorage to activities array
+        localStorage.setItem("activities", JSON.stringify(activities));
+    }
+
+    // When save icon is pressed....
+    $(".saveBtn").click(function(event) {
+        event.preventDefault();
+
+        console.log(userEntry.val());
+
+        var activityText = userEntry.val().trim();
+
+        //Return from function early if submitted activityText is blank
+        if (activityText === "") {
+            alert("Please enter activity in text box before clicking save.");
+            return;
+        }
+
+        // Add new activityText to activities array
+        activities.push(activityText);
+
+        // Store update activities in localStorage, re-render the list
+        storeActivities();
+        renderActivities();
+    });
+    
+
+// userEntry.keyup(function saveToStorage() {
+//     userEntry.attr("style", "background:blue");
+//     localStorage.setItem("hour", JSON.stringify(userEntry.value));
+//     console.log(userEntry.value);
+
+
+})
+
 // TODO: make an object that contains HOUR and ACTIVITY
-console.log();
+  // create user object from submission
+//   let calendar24H = index + 7;
+
+//   var user = {
+    // hourColumn: calendar24H.value,
+    // userActivity: userActivityInput.value.trim(),
+//   };
+
+//   console.log(user);
+
 
 
 // TODO: Function to be able to click to save the new event
-$(".saveBtn").click(function save() {
-    alert("this is save btn clicked");
-    localStorage.setItem("activity")
-});
 
 // TODO: Be able to refresh the page and the saved data persists. 
 
 
-});
+// });
